8a6cc4dc374cb42a7a4a56ec7e31c2e42542ed75
Erica <48196721+ericagong@users.noreply.github.com>
2025-05-13 09:49:35 +0900

    feat: Counter 구현

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,11 +4,39 @@ describe('ui-counter', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('+ 버튼 클릭시 1 증가한다.', () => {
-    // btn-inc 클래스를 가진 요소를 클릭
-    cy.get('.btn-inc').click();
+  it('생성 시 버튼과 초기값(10)을 렌더링한다.', () => {
+    cy.get('.btn-dec').should('exist');
+    cy.get('.btn-inc').should('exist');
+    cy.get('.count-display').should('have.value', '10');
+  });
 
-    // value 클래스를 가진 요소의 텍스트가 10
-    cy.get('.value').should('have.text', '10');
+  it('+ 버튼 클릭시 count가 1 증가한다.', () => {
+    cy.get('.btn-inc').click();
+    cy.get('.count-display').should('have.value', '11');
+  });
+
+  it('- 버튼 클릭 시 count가 1 감소한다.', () => {
+    cy.get('.btn-dec').click();
+    cy.get('.count-display').should('have.value', '9');
+  });
+
+  it('+ 버튼을 눌렀을 때 count가 12가 넘는 경우 더이상 증가하지 못한다. (Max 값이 12)', () => {
+    cy.get('.btn-inc').click();
+    cy.get('.btn-inc').click();
+    cy.get('.btn-inc').click();
+    cy.get('.count-display').should('have.value', '12');
+
+    cy.get('.btn-inc').click();
+    cy.get('.count-display').should('have.value', '12');
+  });
+
+  it(' 버튼을 눌렀을 때 count는 8보다 작아지는 경우 감소하지 못한다. (Min 값이 8)', () => {
+    cy.get('.btn-dec').click();
+    cy.get('.btn-dec').click();
+    cy.get('.btn-dec').click();
+    cy.get('.count-display').should('have.value', '8');
+
+    cy.get('.btn-dec').click();
+    cy.get('.count-display').should('have.value', '8');
   });
 });
--- src/js/modules/Counter.js
+++ src/js/modules/Counter.js
@@ -1,18 +1,38 @@
 export default function Counter({ $app }) {
+  const increase = () => {
+    const $countDisplay = document.querySelector('.count-display');
+    const currCount = Number($countDisplay.value);
+    if (currCount >= 12) return;
+    $countDisplay.value = currCount + 1;
+  };
+
+  const decrease = () => {
+    const $countDisplay = document.querySelector('.count-display');
+    const currCount = Number($countDisplay.value);
+    if (currCount <= 8) return;
+    $countDisplay.value = currCount - 1;
+  };
+
   const render = () => {
     $app.innerHTML = `
        <div class="container">
           <h1>ui counter</h1>
           <div class="counter">
-            <a href="#" class="minus-button"><span>-</span></a>
+            <a href="#" class="btn-dec"><span>-</span></a>
             <input name="count" type="text" class="count-display" value="10">
-            <a href="#" class="plus-button"><span>+</span></a>
+            <a href="#" class="btn-inc"><span>+</span></a>
           </div>
         </div>`;
   };
 
   const init = () => {
     render();
+
+    const $decBtn = document.querySelector('.btn-dec');
+    $decBtn.addEventListener('click', decrease);
+
+    const $incBtn = document.querySelector('.btn-inc');
+    $incBtn.addEventListener('click', increase);
   };
 
   init();
